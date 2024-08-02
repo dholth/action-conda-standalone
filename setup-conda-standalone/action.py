0d76070ec3e175704538ad61dfac18f2108f77ed
Daniel Holth <dholth@anaconda.com>
2024-08-02 16:56:52 -0400

    allow target directory to exist

--- setup-conda-standalone/action.py
+++ setup-conda-standalone/action.py
@@ -38,7 +38,7 @@ def main():
     pprint.pprint(dict(os.environ))
 
     output_path = Path(os.environ['RUNNER_TEMP'], "conda-standalone", "conda.exe")
-    output_path.parent.mkdir(parents=True)
+    output_path.parent.mkdir(parents=True, exist_ok=True)
 
     print("Ensure", output_path)
 
